2c2cb37277806d090b1c620a73b6eba673da6127
Jihoo Kim <jihoo94@gmail.com>
2023-03-11 04:15:14 +0900

    Finished Gatsby Tutorial part 4

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,28 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import {
   container,
   navLink,
   heading,
   navLinkItem,
   navLinkText,
+  siteTitle,
 } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLink}>
           <li className={navLinkItem}>
@@ -18,9 +30,14 @@ const Layout = ({ pageTitle, children }) => {
               Home
             </Link>
           </li>
-          <li>
+          <li className={navLinkItem}>
             <Link to="/about" className={navLinkText}>
               About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
             </Link>
           </li>
         </ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,17 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
+import Seo from "../components/seo";
 
 const IndexPage = () => {
   return (
-    <main>
-      <Layout pageTitle="Home Page">
-        <p>I'm making this by following the Gatsby Tutorial.</p>
-        <StaticImage src="../images/dog.jpeg" alt="dog" />
-      </Layout>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage src="../images/dog.jpeg" alt="dog" />
+    </Layout>
   );
 };
 
-export const Head = () => <title>Home Page</title>;
+export const Head = () => <Seo title="Home Page" />;
 
 export default IndexPage;
